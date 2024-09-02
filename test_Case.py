0712b3868b6991296ea8da744a14b5cbf830b680
Bhavesh Kumar Singh <bhavesh.singh@non.keysight.com>
2024-09-02 17:14:48 +0530

    Added new test case: two number separated by comma.

--- test_Case.py
+++ test_Case.py
@@ -8,3 +8,6 @@ def test_single_number_in_string():
     string = "2"
     assert (addCalculator(string) == 2)
 
+def test_two_number_in_string_with_comma_delimiter():
+    string = "1,2"
+    assert (addCalculator(string) == 3)
